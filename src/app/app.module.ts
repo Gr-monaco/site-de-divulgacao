d84d69ae1e86cd5f9ad2ad86c70152d52ff7b18c
Gabriel Monaco <gr-monaco@hotmail.com>
2021-09-29 19:07:21 -0300

    Atualizar dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainWebsiteBodyComponent } from './main-website-body/main-website-body.component';
+import { ButtonBarComponent } from './button-bar/button-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainWebsiteBodyComponent,
+    ButtonBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
