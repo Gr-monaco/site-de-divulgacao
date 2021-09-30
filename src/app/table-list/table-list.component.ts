d688445710bc9fb3ac52e72fcca9685fd2e84add
Gabriel Monaco <gr-monaco@hotmail.com>
2021-09-30 15:24:52 -0300

    Adicionar cores dependendo da preferencia
    Foi adicionado uma função que retorna a classe css que contem a cor da
    preferencia do usuario, que sera mostradada na lista.

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -10,6 +10,22 @@ export class TableListComponent implements OnInit {
 
   constructor(public userProfileService : UserProfileService) { }
 
+  getClassOfUserPref(userProfileService: {preferencia : string}){    // O codigo funciona, entretanto, utilizaria um swith, devo pesquisar como utilizar switch em typescript
+    return {
+      'pref-csharp': userProfileService.preferencia == 'C#',
+      'pref-cplusplus': userProfileService.preferencia == 'C++',
+      'pref-c': userProfileService.preferencia == 'C',
+      'pref-python': userProfileService.preferencia == 'Python',
+      'pref-php': userProfileService.preferencia == 'PHP',
+      'pref-html': userProfileService.preferencia == 'HTML',
+      'pref-javascript': userProfileService.preferencia == 'Javascript',
+      'pref-ruby': userProfileService.preferencia == 'Ruby',
+      'pref-r': userProfileService.preferencia == 'R',
+      'pref-lisp': userProfileService.preferencia == 'Lisp',
+      'pref-java': userProfileService.preferencia == 'Java'
+    }
+  }
+
   ngOnInit(): void {
   }
 
